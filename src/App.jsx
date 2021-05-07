9529c3acf35803d6345fd3b117a99e81c2d1caca
icortez <icortez@ufg.edu.sv>
2021-05-07 10:36:24 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Navbar, Sidebar, Footer } from './components';
+
+const App = () => {
+  return (
+    <>
+      <h2>app.jsx</h2>
+    </>
+  );
+};
+
+export default App;
