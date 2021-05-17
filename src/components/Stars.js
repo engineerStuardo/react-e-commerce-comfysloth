7bfe09a42cc71efcc58fb3499dbdfb9161c8d030
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-05-17 05:56:12 -0600

    building stars component in single product page

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from 'react';
+import styled from 'styled-components';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+
+const Stars = ({ stars, reviews }) => {
+  const tmpStars = Array.from({ length: 5 }, (_, index) => {
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <BsStarFill />
+        ) : stars >= index + 0.5 ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
+
+  console.log(tmpStars)
+
+  return (
+    <Wrapper>
+      <div className='stars'>{tmpStars}</div>
+      <p className='reviews'>({reviews} customer reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -18,5 +40,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
