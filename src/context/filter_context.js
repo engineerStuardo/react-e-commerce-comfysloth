7dbc37031055ce4121c561b20e461ba00b9b1042
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-05-18 05:39:14 -0600

    bulding filter conttext and reducer and adding the filter provider to index js

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useContext, useReducer } from 'react';
+
 import reducer from '../reducers/filter_reducer';
+import { useProductsContext } from './custom_hooks';
 import {
   LOAD_PRODUCTS,
   SET_GRIDVIEW,
@@ -10,13 +12,22 @@ import {
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
 } from '../actions';
-import { useProductsContext } from './products_context';
 
-const initialState = {};
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+};
 
 export const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
   return (
     <FilterContext.Provider value='filter context'>
       {children}
