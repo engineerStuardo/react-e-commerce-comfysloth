1e75e6af9dcc4f7e4fb209b4dd6f1967bf966b47
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-05-20 04:37:41 -0600

    building company filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -48,6 +48,21 @@ const Filters = () => {
                 </button>
               ))}
             </div>
+            <div className='form-control'>
+              <h5>company</h5>
+              <select
+                name='company'
+                value={company}
+                onChange={updateFilters}
+                className='company'
+              >
+                {companies.map((companiesItem, index) => (
+                  <option key={index} value={companiesItem}>
+                    {companiesItem}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
         </form>
       </div>
