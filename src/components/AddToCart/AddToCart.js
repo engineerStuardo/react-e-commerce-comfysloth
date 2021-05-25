e76cfe82b69bf17146ff1b51288ff34a9c99c74d
icortez <icortez@ufg.edu.sv>
2021-05-25 08:35:54 -0600

    adding add to cart function and stablish it in AddToCart component

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -6,7 +6,10 @@ import { Wrapper } from './styled';
 import { useCartContext } from '../../context/custom_hooks';
 import AmountButtons from '../AmountButtons/AmountButtons';
 
-const AddToCart = ({ product: { id, stock, colors } }) => {
+const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
+
+  const { id, stock, colors } = product;
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
 
@@ -49,7 +52,11 @@ const AddToCart = ({ product: { id, stock, colors } }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to='/cart' className='btn'>
+        <Link
+          to='/cart'
+          className='btn'
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
