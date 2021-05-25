211c6b38ae709c1ea74ca2b90b7e8992165ba778
icortez <icortez@ufg.edu.sv>
2021-05-25 09:12:36 -0600

    building add to cart method in cart reducer so the user can add a product

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,49 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from '../actions';
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  switch (action.type) {
+    case ADD_TO_CART:
+      const { id, color, amount, product } = action.payload;
+      const tmpItem = state.cart.find(i => i.id === id + color);
+      if (tmpItem) {
+        const tmpCart = state.cart.map(cartItem => {
+          if (cartItem.id === id + color) {
+            let newAmount = cartItem.amount + amount;
+            if (newAmount > cartItem.max) newAmount = cartItem.max;
+            return {
+              ...cartItem,
+              amount: newAmount,
+            };
+          } else {
+            return cartItem;
+          }
+        });
+        return {
+          ...state,
+          cart: tmpCart,
+        };
+      } else {
+        const newItem = {
+          id: id + color,
+          name: product.name,
+          color,
+          amount,
+          image: product.images[0].url,
+          price: product.price,
+          max: product.stock,
+        };
+        return {
+          ...state,
+          cart: [...state.cart, newItem],
+        };
+      }
 
-export default cart_reducer
+    default:
+      throw new Error(`No Matching "${action.type}" - action type`);
+  }
+};
+
+export default cart_reducer;
